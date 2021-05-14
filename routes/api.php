7b44b357e8da5f9503fca9ea82621081ef9583bd
Kristian Orozco <korozco@lean-tech.io>
2021-05-13 20:26:05 -0500

    all stages

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Page;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/posts', function (Request $request){
+    $posts = Page::all();
+    return response()->json($posts);
 });
